4f98c600162c3b806366e7e927bbaea3f14f8a23
Ahmed <qahmed89@gmail.com>
2020-08-27 00:05:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglisttesting/RegistrationUtil.kt
@@ -0,0 +1,32 @@
+package com.example.shoppinglisttesting
+
+object RegistrationUtil {
+    private val existingUserName = listOf("peter","carl")
+    /**
+     * the input is  not valid  is empty
+     * ... the username and password must not empty
+     * ... the username is already taken
+     * ... the comfirmedPassword is not the same as the real password
+     * ... the password contains less than 2 digits
+     */
+
+    fun validateRegistrationInput(
+        username:String,
+        password :String,
+        confirmedPassword :String
+    ): Boolean{
+           if ( username.isEmpty() || password.isEmpty()){
+               return false
+           }
+        if (username in existingUserName){
+            return false
+        }
+        if (password != confirmedPassword){
+            return false
+        }
+        if (password.count { it.isDigit()}<2) {
+            return false
+        }
+        return true
+    }
+}
