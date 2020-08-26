b137684e49694c715f32828657abf25147e6d158
Ahmed <qahmed89@gmail.com>
2020-08-27 01:23:45 +0200

    Inital RoomDatabase

---
+++ app/src/main/java/com/example/shoppinglisttesting/data/local/ShoppingDao.kt
@@ -0,0 +1,19 @@
+package com.example.shoppinglisttesting.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    @Delete
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_item")
+    fun observeAllShoppingItem(): LiveData<List<ShoppingItem>>
+
+    @Query("SELECT SUM(price * amount) FROM shopping_item")
+    fun observeTotalPrice(): LiveData<Float>
+}
