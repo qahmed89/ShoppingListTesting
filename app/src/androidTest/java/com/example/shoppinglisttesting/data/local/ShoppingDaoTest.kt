2d3614a3475af76d2dee225cb188fee0ef1dceee
Ahmed <qahmed89@gmail.com>
2020-09-09 03:25:44 +0200

    Testing Fragment with Dagger Hilts

--- app/src/androidTest/java/com/example/shoppinglisttesting/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/example/shoppinglisttesting/data/local/ShoppingDaoTest.kt
@@ -3,6 +3,8 @@ package com.example.shoppinglisttesting.data.local
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.SmallTest
 import com.example.shoppinglisttesting.getOrAwaitValue
+import com.example.shoppinglisttesting.launchFragmentInHiltContainer
+import com.example.shoppinglisttesting.ui.fragment.ShoppingFragment
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -42,6 +44,14 @@ class ShoppingDaoTest {
     @After
     fun teardown() {
         dataBase.close()
+    }
+
+    @Test
+    fun testLaunchFragmentInHiltContainer() {
+        launchFragmentInHiltContainer<ShoppingFragment> {
+
+        }
+
     }
 
     @Test
